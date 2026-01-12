110a7d962aa8b505b33c48c10ffa436572013d95
samuhlo <samu13lop@gmail.com>
2026-01-12 17:14:22 +0100

    frist commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+    import gsap from 'gsap';
+    import { ScrollTrigger, SplitText } from 'gsap/all';
+    import HelloWorld from "./components/HelloWorld.vue";
+
+    gsap.registerPlugin(ScrollTrigger, SplitText);
+</script>
+
+<template>
+    <div class="flex-center h-[100vh]">
+        <HelloWorld />
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="text-3xl text-indigo-300">Hola, GSAP !!</div>
+</template>
+
+
